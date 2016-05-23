42f7aa366daf2e8e9d842cc59005a24d1d05889a
Cukia <cukiakimani@gmail.com>
2016-05-23 03:36:12 +0200

    drawing pixel brush

--- Assets/Editor/PixelCanvas.cs
+++ Assets/Editor/PixelCanvas.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PixelCanvas : EditorWindow
 {
+    Texture _alphaTexture;
+	
     // Add menu item named "Pixel Canvas" to the Window menu
     [MenuItem("Window/Pixel Canvas")]
     public static void ShowWindow()
@@ -11,9 +13,35 @@ public class PixelCanvas : EditorWindow
         //Show existing window instance. If one doesn't exist, make one.
         EditorWindow.GetWindow(typeof(PixelCanvas));
     }
+
+    void OnEnable()
+    {
+        _alphaTexture = Resources.Load<Texture2D>("alpha_spriteDeformer");
+    }
+
+    void Update()
+    {
+    	Repaint();
+    }
     
     void OnGUI()
     {
-        
+    	// draw canvas
+    	float zoom = 10f;
+    	int brushSize = 2;
+    	Vector2 canvasSize = new Vector2(64, 64);
+        Rect canvasRect = new Rect(Vector2.zero, canvasSize * zoom);
+        GUI.DrawTextureWithTexCoords(canvasRect, _alphaTexture, new Rect(0, 0, canvasSize.x / 2, canvasSize.y / 2));
+    
+        Event e = Event.current;
+
+        if (canvasRect.Contains(e.mousePosition))
+        {
+        	Vector2 pos = new Vector2(e.mousePosition.x, e.mousePosition.y);
+        	Vector2 size = Vector2.one * brushSize * zoom;
+        	EditorGUI.DrawRect(new Rect(pos, size), Color.black);
+        }
     }
+
+
 }
